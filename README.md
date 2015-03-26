aa6fd31dac835abfcc244bb43e39bc088fdd5506
Geert-Johan Riemer <geertjohan@geertjohan.net>
2015-03-26 13:35:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## pwdip
+**Print Working Directory Import Path** is a tool that prints the Go import path based on the working directory.
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"go/build"
+	"os"
+)
+
+func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to get working directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	pkg, err := build.ImportDir(wd, build.FindOnly)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "can not find import path: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Print(pkg.ImportPath)
+}
