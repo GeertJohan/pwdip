da389deaac9699866bb6feb364b994cb28976c46
Geert-Johan Riemer <geertjohan@geertjohan.net>
2015-03-26 14:50:05 +0100

    Reject local imports ("."). Don't verify the package.

--- main.go
+++ main.go
@@ -13,9 +13,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	pkg, err := build.ImportDir(wd, build.ImportComment)
+	pkg, err := build.ImportDir(wd, build.FindOnly)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "can not find import path: %v\n", err)
+		os.Exit(1)
+	}
+
+	if pkg.ImportPath == "." {
+		fmt.Fprint(os.Stderr, "package is not located in GOPATH\n")
 		os.Exit(1)
 	}
 
