b9bdd35d748d31ae9251d3f8b7666160ba9d50f6
Geert-Johan Riemer <geertjohan@geertjohan.net>
2015-03-26 14:58:07 +0100

    Prefix stderr output with "error: "

--- main.go
+++ main.go
@@ -9,18 +9,18 @@ import (
 func main() {
 	wd, err := os.Getwd()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to get working directory: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error: failed to get working directory: %v\n", err)
 		os.Exit(1)
 	}
 
 	pkg, err := build.ImportDir(wd, build.FindOnly)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "can not find import path: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error: can not find import path: %v\n", err)
 		os.Exit(1)
 	}
 
 	if pkg.ImportPath == "." {
-		fmt.Fprint(os.Stderr, "package is not located in GOPATH\n")
+		fmt.Fprint(os.Stderr, "error: package is not located in GOPATH\n")
 		os.Exit(1)
 	}
 
